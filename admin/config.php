ae40e173c5ed77b1594d6b8203bec0b6aea5b148
An Luu <lta.anluu@gmail.com>
2017-05-07 10:25:18 +0700

    fix deploy

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://opencart-the-mark.herokuapp.com/admin/');
+define('HTTP_CATALOG', 'https://opencart-the-mark.herokuapp.com/');
+// HTTPS
+define('HTTPS_SERVER', 'https://opencart-the-mark.herokuapp.com/admin/');
+define('HTTPS_CATALOG', 'https://opencart-the-mark.herokuapp.com/');
+
+// DIR
+define('DIR_APPLICATION', __DIR__ . '/../admin/');
+define('DIR_SYSTEM', __DIR__ . '/../system/');
+define('DIR_IMAGE', __DIR__ . '/../image/');
+define('DIR_DATABASE', __DIR__ . '/../system/database/');
+define('DIR_LANGUAGE', __DIR__ . '/../admin/language/');
+define('DIR_TEMPLATE', __DIR__ . '/../admin/view/template/');
+define('DIR_CONFIG', __DIR__ . '/../system/config/');
+define('DIR_CACHE', __DIR__ . '/../system/storage/cache/');
+define('DIR_DOWNLOAD', __DIR__ . '/../system/storage/download/');
+define('DIR_LOGS', __DIR__ . '/../system/storage/logs/');
+define('DIR_CATALOG', __DIR__ . '/../catalog/');
+define('DIR_MODIFICATION', DIR_SYSTEM . 'storage/modification/');
+define('DIR_UPLOAD', __DIR__ . '/../system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'us-cdbr-iron-east-03.cleardb.net');
+define('DB_USERNAME', 'bfa995dd17149c');
+define('DB_PASSWORD', 'cb03d001');
+define('DB_DATABASE', 'heroku_9aaef6cd1a8b63e');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
+?>
